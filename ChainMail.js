36728b11788e36c04db979ec4d0e71e5b1666248
Nicolas <theendermengamer@gmail.com>
2015-02-17 13:53:36 +0100

    Create ChainMail.js

---
+++ ChainMail.js
@@ -0,0 +1,62 @@
+/*
+If anyone share you need to put forum link or the link of my channel of Youtube <https://www.youtube.com/channel/UCk9mM0GhKVfu0Se_8NX6Miw>
+EnderGamer Script (C) GNC copyright
+Copyright (C) <2014>  <EnderGamer>
+
+This work is licensed under the Creative Commons Attribution- NonCommercial 4.0 International License. To view a copy
+of this license, visit http://creativecommons.org/licenses/by-nc/4.0/ or send a letter to Creative Commons, 444 Castro
+Street, Suite 900, Mountain View, California, 94041, USA.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/* ******* ChainMail Armor Mod By TheEndermenGamer ******* */
+
+//Helmet
+
+Item.addShapedRecipe(302, 1, 0, [
+"sss",
+"sds"
+], 
+["s", 51, 0]);
+
+//ChestPlate
+
+Item.addShapedRecipe(303, 1, 0, [
+"sds",
+"sss",
+"sss"
+], 
+["s", 51, 0]);
+
+//Leggings
+
+Item.addShapedRecipe(304, 1, 0, [
+"sss",
+"sds",
+"sds"
+], 
+["s", 51, 0]);
+
+//Boots
+
+Item.addShapedRecipe(305, 1, 0, [
+"ddd",
+"sds",
+"sds"
+], 
+["s", 51, 0]);
+
+//Fire Craft
+
+Item.addShapedRecipe(51, 1, 0, [
+"dsd",
+"tdd"
+], 
+["s", 263, 0, "t",259,0]);
